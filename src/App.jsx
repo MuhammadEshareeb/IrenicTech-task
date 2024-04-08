20a6a09ba5eab91d1d9633c1bfb8ab18cf3814ce
MuhammadEshareeb <eshareebm@gmail.com>
2024-04-08 11:38:30 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.css';
+import ChatGptDark1 from './pages/ChatGptDark1';
+
+
+
+export default function App() {
+  return (
+    <ChatGptDark1/>
+  );
+}
---
+++ src/pages/ChatGptDark1.jsx
@@ -0,0 +1,407 @@
+import './ChatGptDark1.css'
+
+export default function ChatGptDark1() {
+  return (
+    <div className="chat-gpt-dark-1">
+      <div className="landscape">
+      </div>
+      <div className="main-bg">
+        <div className="left-panel">
+          <div className="logo">
+          </div>
+          <div className="container-32">
+            <img className="marker" src="assets/vectors/Marker_x2.svg" />
+            <div className="chat">
+              <div className="shadow">
+              </div>
+              <div className="bg">
+                <img className="vector-21" src="assets/vectors/Vector5_x2.svg" />
+              </div>
+            </div>
+          </div>
+          <div className="manage-subscription">
+            <img className="filter-1" src="assets/vectors/Filter_x2.svg" />
+          </div>
+          <div className="explore-whats-new">
+            <img className="compass" src="assets/vectors/Compass_x2.svg" />
+          </div>
+          <div className="settings">
+            <div className="settings-icon">
+              <img className="layer-1" src="assets/vectors/Layer1_x2.svg" />
+            </div>
+          </div>
+          <div className="user-profile">
+            <div className="dall-e-2022111113441">
+            </div>
+            <div className="frame-38">
+              <span className="pro">
+              PRO
+              </span>
+            </div>
+          </div>
+          <div className="logout">
+            <img className="signout" src="assets/vectors/Signout_x2.svg" />
+          </div>
+          <div className="divider-left-most-panel">
+          </div>
+          <div className="light-dark-version">
+            <div className="frame-36">
+            </div>
+            <img className="vector-12" src="assets/vectors/Vector2_x2.svg" />
+            <div className="frame-37">
+            </div>
+            <img className="vector-13" src="assets/vectors/Vector3_x2.svg" />
+          </div>
+        </div>
+        <div className="my-chat-section">
+          <div className="container-4">
+            <div className="my-chats">
+            My Chats
+            </div>
+            <div className="container-19">
+              <img className="new-chat-icon" src="assets/vectors/NewChatIcon_x2.svg" />
+              <img className="kebab-menu-1" src="assets/vectors/KebabMenu1_x2.svg" />
+            </div>
+          </div>
+          <div className="chat-tab">
+            <div className="tab-ber-bg">
+              <img className="vector-4" src="assets/vectors/Vector4_x2.svg" />
+              <div className="saved">
+              SAVED
+              </div>
+              <div className="number">
+                <span className="container">
+                24
+                </span>
+              </div>
+            </div>
+            <div className="chats-shadow">
+            </div>
+            <div className="chats">
+              <img className="vector-5" src="assets/vectors/Vector7_x2.svg" />
+              <div className="chats-1">
+              CHATS
+              </div>
+              <div className="number-1">
+                <span className="container-1">
+                24
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="container-15">
+            <div className="search-bar">
+              <div className="search-icon-1">
+                <img className="search" src="assets/vectors/Search_x2.svg" />
+              </div>
+              <span className="search-1">
+              Search...
+              </span>
+            </div>
+            <div className="filter">
+              <img className="icon" src="assets/vectors/Icon2_x2.svg" />
+            </div>
+          </div>
+          <div className="list-1">
+            <div className="container-27">
+              <div className="container">
+                <img className="union" src="assets/vectors/Union_x2.svg" />
+                <div className="cosmic-evolution">
+                Cosmic Evolution
+                </div>
+              </div>
+              <div className="pm">
+              9:34 PM
+              </div>
+            </div>
+            <span className="some-15-billion-years-ago-the-universe-emerged-from-ahot-dense-sea-of">
+            Some 15 billion years ago the universe emerged from a hot, dense sea of...
+            </span>
+          </div>
+          <div className="list-2">
+            <div className="container-9">
+              <div className="container-21">
+                <div className="image-295">
+                </div>
+                <span className="warning-messages-samples-1">
+                Warning Messages Samples
+                </span>
+              </div>
+              <div className="now">
+              Now
+              </div>
+            </div>
+            <span className="sure-here-are-three-different-versions-of-404-error-messages-for-an-ecommerce">
+            Sure! Here are three different versions of 404 error messages for an ecommerce ...
+            </span>
+          </div>
+          <div className="list-3">
+            <div className="container-13">
+              <div className="container-24">
+                <div className="image-294">
+                </div>
+                <span className="competitive-analysis-research">
+                Competitive Analysis research
+                </span>
+              </div>
+              <div className="thu">
+              Thu
+              </div>
+            </div>
+            <span className="acompetitive-analysis-of-restaurant-delivery-mobile-applications-reveals-key-insights">
+            A competitive analysis of restaurant delivery mobile applications reveals key insights ...
+            </span>
+          </div>
+          <div className="list-4">
+            <div className="container-30">
+              <div className="container-26">
+                <div className="image-293">
+                </div>
+                <span className="user-personas-research">
+                User Personas Research
+                </span>
+              </div>
+              <div className="mon">
+              Mon
+              </div>
+            </div>
+            <span className="user-persona-research-is-aprocess-of-creating-fictional-but-realistic-representati">
+            User persona research is a process of creating fictional but realistic representati...
+            </span>
+          </div>
+          <div className="list-5">
+            <div className="container-25">
+              <div className="container-10">
+                <div className="image-292">
+                </div>
+                <span className="call-to-action-texts">
+                Call To Action texts
+                </span>
+              </div>
+              <div className="oct-1">
+              17 Oct
+              </div>
+            </div>
+            <span className="here-are-afew-examples-of-cta-button-text-get-started-now">
+            here are a few examples of CTA button text<br />
+            &#34;Get started now&#34; ...
+            </span>
+          </div>
+          <div className="list-6">
+            <div className="container-34">
+              <div className="container-5">
+                <div className="image-291">
+                </div>
+                <span className="video-script-intros">
+                Video Script Intros
+                </span>
+              </div>
+              <div className="oct">
+              10 Oct
+              </div>
+            </div>
+            <span className="hi-iminsert-name-here-and-im-on-my-way-to-prague-one-of-the-most-beautiful">
+            Hi, I&#39;m [insert name here], and I&#39;m on my way to Prague, one of the most beautiful...
+            </span>
+          </div>
+          <div className="list-7">
+            <div className="container-7">
+              <div className="container-1">
+                <div className="image-29">
+                </div>
+                <span className="ux-of-abanana">
+                UX of a Banana
+                </span>
+              </div>
+              <div className="aug">
+              21 Aug
+              </div>
+            </div>
+            <span className="are-you-sure-you-want-to-delete-this-file-this-action-cannot-be-undone-and-the-file">
+            Are you sure you want to delete this file? This action cannot be undone and the file...
+            </span>
+            <div className="rectangle-1">
+            </div>
+          </div>
+        </div>
+        <div className="right-panel">
+          <div className="container-6">
+            <div className="warning-messages-samples">
+            Warning Messages Samples
+            </div>
+            <div className="container-29">
+              <img className="search-icon" src="assets/vectors/SearchIcon_x2.svg" />
+              <img className="kebab-menu" src="assets/vectors/KebabMenu_x2.svg" />
+            </div>
+          </div>
+          <div className="chat-window-bg">
+            <div className="chat-1">
+              <div className="container-17">
+                <span className="you">
+                You
+                </span>
+                <div className="sep-1130-pm">
+                24 Sep ▪ 11:30 PM
+                </div>
+              </div>
+              <div className="speech-bubble-1">
+                <span className="how-do-you-define-usability-testing-in-ux-design">
+                How do you define usability testing in UX design?
+                </span>
+                <img className="edit-1" src="assets/vectors/Edit1_x2.svg" />
+              </div>
+              <div className="user-avatar-1">
+              </div>
+            </div>
+            <div className="chat-2">
+              <div className="container-3">
+                <div className="container-12">
+                  <span className="response-1">
+                  Response
+                  </span>
+                  <div className="sep-1130-pm-1">
+                  24 Sep ▪ 11:30 PM
+                  </div>
+                </div>
+                <div className="container-8">
+                  <div className="arrow-left">
+                    <img className="icon-2" src="assets/vectors/Icon_x2.svg" />
+                  </div>
+                  <div className="container-2">
+                  1/3
+                  </div>
+                  <div className="arrow-right">
+                    <img className="icon-1" src="assets/vectors/Icon1_x2.svg" />
+                  </div>
+                </div>
+              </div>
+              <div className="speech-bubble-2">
+                <div className="usability-testing-is-atechnique-used-in-user-experience-ux-design-to-evaluate-aproduct-or-service-by-testing-it-with-representative-users-the-purpose-of-usability-testing-is-to-identify-any-usability-problems-collect-quantitative-and-qualitative-data-on-users-experiences-and-determine-the-overall-user-satisfaction-with-the-product-or-service">
+                Usability testing is a technique used in user experience (UX) design to evaluate a product or service by testing it with representative users. The purpose of usability testing is to identify any usability problems, collect quantitative and qualitative data on users&#39; experiences, and determine the overall user satisfaction with the product or service.
+                </div>
+                <div className="container-2">
+                  <div className="feedback-1">
+                    <img className="smiley" src="assets/vectors/Smiley_x2.svg" />
+                    <img className="smiley-sad-2" src="assets/vectors/SmileySad1_x2.svg" />
+                  </div>
+                  <div className="container-23">
+                    <div className="generate-response-2">
+                      <img className="union-2" src="assets/vectors/Union2_x2.svg" />
+                      <div className="generate-response-3">
+                      Generate Response
+                      </div>
+                    </div>
+                    <div className="copy-2">
+                      <img className="vector-35" src="assets/vectors/Vector6_x2.svg" />
+                      <span className="copy-3">
+                      Copy
+                      </span>
+                    </div>
+                    <div className="save-1">
+                      <img className="vector-34" src="assets/vectors/Vector10_x2.svg" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="chat-gpt-avatar-1">
+              </div>
+            </div>
+            <div className="container-33">
+              <div className="line-right">
+              </div>
+              <span className="today">
+              Today
+              </span>
+              <div className="line-left">
+              </div>
+            </div>
+            <div className="chat-3">
+              <div className="container-20">
+                <span className="you-1">
+                You
+                </span>
+                <div className="min-ago">
+                1 min ago
+                </div>
+              </div>
+              <div className="speech-bubble-3">
+                <span className="youre-aux-writer-now-generate-3-versions-of-404-error-messages-for-aecommerce-clothing-website">
+                you&#39;re a UX writer now. Generate 3 versions of 404 error messages for a ecommerce clothing website.
+                </span>
+                <img className="edit" src="assets/vectors/Edit_x2.svg" />
+              </div>
+              <div className="user-avatar-3">
+              </div>
+            </div>
+            <div className="chat-4">
+              <div className="container-18">
+                <span className="response">
+                Response
+                </span>
+                <div className="just-now">
+                Just now
+                </div>
+              </div>
+              <div className="speech-bubble-4">
+                <div className="sure-here-are-three-different-versions-of-404-error-messages-for-an-ecommerce-clothing-website-uh-oh-it-looks-like-the-page-youre-looking-for-isnt-here-please-check-the-url-and-try-again-or-return-to-the-homepage-to-continue-shopping-2-whoops-we-cant-seem-to-find-the-page-youre-looking-for-please-double-check-the-url-or-use-our-search-bar-to-find-what-you-need-you-can-also-browse-our-collection-of-stylish-clothes-and-accessories-3-sorry-the-page-youre-trying-to-access-isnt-available-its-possible-that-the-item-has-sold-out-or-the-page-has-been-removed-please-click-back-to-return-to-the-previous-page-or-head-over-to-our-homepage-to-explore-more">
+                Sure! Here are three different versions of 404 error messages for an ecommerce clothing website:<br />
+                <br />
+                Uh-oh! It looks like the page you&#39;re looking for isn&#39;t here. Please check the URL and try again or return to the homepage to continue shopping.<br />
+                <br />
+                2. Whoops! We can&#39;t seem to find the page you&#39;re looking for. Please double-check the URL or use our search bar <br />
+                    to find what you need. You can also browse our collection of stylish clothes and accessories.<br />
+                <br />
+                3. Sorry, the page you&#39;re trying to access isn&#39;t available. It&#39;s possible that the item has sold out or the page has <br />
+                    been removed. Please click back to return to the previous page or head over to our homepage to explore more.
+                </div>
+                <div className="container-16">
+                  <div className="feedback">
+                    <img className="smiley-sad" src="assets/vectors/SmileySad_x2.svg" />
+                    <img className="smiley-sad-1" src="assets/vectors/SmileySad2_x2.svg" />
+                  </div>
+                  <div className="container-31">
+                    <div className="generate-response">
+                      <img className="union-1" src="assets/vectors/Union1_x2.svg" />
+                      <span className="generate-response-1">
+                      Generate Response
+                      </span>
+                    </div>
+                    <div className="copy">
+                      <img className="vector-30" src="assets/vectors/Vector_x2.svg" />
+                      <span className="copy-1">
+                      Copy
+                      </span>
+                    </div>
+                    <div className="save">
+                      <img className="vector-31" src="assets/vectors/Vector9_x2.svg" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="chat-gpt-avatar-2">
+              </div>
+            </div>
+            <div className="container-11">
+              <div className="input-textbox">
+                <div className="container-14">
+                  <div className="magic-wand-tool">
+                    <img className="container-28" src="assets/vectors/Container_x2.svg" />
+                  </div>
+                  <div className="ask-questions-or-type-for-commands">
+                  Ask questions, or type ‘/’ for commands
+                  </div>
+                </div>
+                <div className="send-icon">
+                  <img className="vector-3" src="assets/vectors/Vector8_x2.svg" />
+                </div>
+              </div>
+              <div className="voice-input-icon">
+                <img className="vector-1" src="assets/vectors/Vector1_x2.svg" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
